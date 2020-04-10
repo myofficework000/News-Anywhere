a720afa9adb6f36e687f09a37372a376efeea06a
Abhishek Pathak <abhishek.pathak@nagarro.com>
2020-04-10 15:32:59 +0530

    Added web view for detail news article.

--- app/src/main/java/com/abhishek/newsapp/Extensions.kt
+++ app/src/main/java/com/abhishek/newsapp/Extensions.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -25,7 +26,25 @@ fun ImageView.load(path: String, request: (RequestCreator) -> RequestCreator) {
     request(context.picasso.load(path)).into(this)
 }
 
+/*
+@RequiresApi(Build.VERSION_CODES.O)
 fun Date.toFormattedString(format: String = "EEE, MMM d, ''yy"): String {
+    val parsedDate = LocalDateTime.parse("2018-12-14T09:55:00", DateTimeFormatter.ISO_DATE_TIME)
     val simpleDateFormat = SimpleDateFormat(format)
     return simpleDateFormat.format(this)
+}
+*/
+
+fun toFormattedString(isoTime: String): String? {
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    var convertedDate: Date? = null
+    var formattedDate: String? = null
+    try {
+        convertedDate = sdf.parse(isoTime)
+        formattedDate = SimpleDateFormat("MMMM dd,yyyy").format(convertedDate)
+    } catch (e: ParseException) {
+        e.printStackTrace()
+    }
+
+    return formattedDate
 }
--- app/src/main/java/com/abhishek/newsapp/adapter/viewholder/NewsArticleViewHolder.kt
+++ app/src/main/java/com/abhishek/newsapp/adapter/viewholder/NewsArticleViewHolder.kt
@@ -1,30 +1,56 @@
 package com.abhishek.newsapp.adapter.viewholder
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import com.abhishek.newsapp.load
 import com.abhishek.newsapp.toFormattedString
 import com.abhishek.newsapp.ui.model.Article
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.layout_news_article_single.view.cardView
 import kotlinx.android.synthetic.main.layout_news_article_single.view.iv_article_image
+import kotlinx.android.synthetic.main.layout_news_article_single.view.progressBar
 import kotlinx.android.synthetic.main.layout_news_article_single.view.tv_article_description
-import kotlinx.android.synthetic.main.layout_news_article_single.view.tv_author
+import kotlinx.android.synthetic.main.layout_news_article_single.view.tv_link
 import kotlinx.android.synthetic.main.layout_news_article_single.view.tv_publish_date
 import kotlinx.android.synthetic.main.layout_news_article_single.view.tv_title
-import java.util.Date
+import kotlinx.android.synthetic.main.layout_news_article_single.view.webViewContent
+import android.webkit.WebViewClient
+
+
+
 
 /**
  * Created by abhishek.pathak on 08/04/2020.
  */
 class NewsArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(article: Article) = with(itemView){
-        tv_author.text = article.author
-        val date : Date? = null
-        tv_publish_date.text = date?.toFormattedString(article.publishedAt!!) ?: article.publishedAt
+    @SuppressLint("SetJavaScriptEnabled")
+    fun bind(article: Article) = with(itemView) {
+        tv_link.setOnClickListener {
+            cardView.visibility = View.GONE
+            webViewContent.visibility = View.VISIBLE
+            progressBar.visibility = View.VISIBLE
+            webViewContent.getSettings().javaScriptEnabled = true
+            webViewContent.webViewClient = WebViewClient()
+            webViewContent.webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: WebView, progress: Int) {
+                    progressBar.progress = progress * 100
+                }
+            }
+            webViewContent.loadUrl(article.url)
+            webViewContent.isHorizontalScrollBarEnabled = false
+        }
+        tv_publish_date.text = toFormattedString(article.publishedAt!!) ?: article.publishedAt
         tv_title.text = article.title
         tv_article_description.text = article.description
-        iv_article_image.load(article.urlToImage!!) { requestCreator -> requestCreator.fit().centerCrop() }
-        Picasso.with(itemView.context).load(article.urlToImage).fit().centerCrop().into(iv_article_image)
+        iv_article_image.load(article.urlToImage!!) { requestCreator ->
+            requestCreator.fit()
+                .centerCrop()
+        }
+        Picasso.with(itemView.context).load(article.urlToImage).fit().centerCrop()
+            .into(iv_article_image)
     }
 }
